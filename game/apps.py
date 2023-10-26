64bc6316f4cb51a315a23322043d525e4da34b15
Ivan Banny <falco.zebrine@gmail.com>
2023-10-26 23:03:17 +0200

    feat(database): implemented db updates when users join/leave lobbies

--- game/apps.py
+++ game/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
+from django.core.management import call_command
 
 
 class GameConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'game'
+
+    def ready(self):
+        call_command('delete_userparties')
--- game/consumers.py
+++ game/consumers.py
@@ -1,7 +1,10 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.auth import get_user
+from .models import Parties, UsersParties
+from django.contrib.auth.models import User
 from channels.layers import get_channel_layer
+from channels.db import database_sync_to_async
 
 
 class LobbyConsumer(AsyncWebsocketConsumer):
@@ -11,14 +14,19 @@ class LobbyConsumer(AsyncWebsocketConsumer):
 
         # Get user from the JWT token sent in the connection request
         self.user = await get_user(self.scope)
+        self.lobby = await database_sync_to_async(lambda: Parties.objects.get(pid=self.lobby_id))()
 
         await self.channel_layer.group_add(self.lobby_group_id, self.channel_name)
         await self.accept()
 
+        await database_sync_to_async(
+            lambda: UsersParties.objects.create(party_id=self.lobby, user_id=self.user, is_admin=False)
+        )()
         # await self.update_lobby_info()
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.lobby_group_id, self.channel_name)
+        await database_sync_to_async(lambda: UsersParties.objects.filter(user_id=self.user).delete())()
 
     async def receive(self, text_data=None, **kwargs):
         text_data_json = json.loads(text_data)
@@ -45,21 +53,30 @@ class LobbyConsumer(AsyncWebsocketConsumer):
                 #     # TODO: if game is not finished - next_round
                 pass
 
+    # TODO: Implement start_game function
     # async def start_game(self):
     #     self.score = 0
 
+    # TODO: Implement next_round function
     # async def next_round(self, event):
     #     message = event["message"]
     #
     #     await self.send(text_data=json.dumps({"type": "chat", "message": message}))
 
+    async def get_usernames(self):
+        usernames = [u.username for u in await User.objects.get(
+            [up.user_id for up in await UsersParties.objects.filter(party_id=self.lobby_id)]
+        )]
+
+        return usernames
+
     async def update_lobby_info(self):
         channel_layer = get_channel_layer()
-        channels_in_group = await channel_layer.group_channels(self.lobby_id)
+        channels = await channel_layer.group_channels(self.lobby_id)
 
-        username_list = [channel.user.username for channel in channels_in_group]
+        username_list = [channel.user.username for channel in channels]
 
-        for channel_name in channels_in_group:
+        for channel_name in channels:
             await self.channel_layer.send(
                 channel_name, {'type': 'update_lobby_info', 'players': username_list}
             )
