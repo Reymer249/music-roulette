6538eae5ff9372ae3063432cb0883eadcff4c773
Ivan Banny <falco.zebrine@gmail.com>
2023-10-26 17:03:29 +0200

    feat(websocket_routing): implemented structure for further development

--- game/consumers.py
+++ game/consumers.py
@@ -1,28 +1,69 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from asgiref.sync import async_to_sync
-from urllib.parse import urlparse, parse_qs
+from channels.auth import get_user
+from channels.layers import get_channel_layer
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
+class LobbyConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_id = self.scope["url_route"]["kwargs"]["room_id"]
-        self.room_group_id = "room_%s" % self.room_id
+        self.lobby_id = self.scope["url_route"]["kwargs"]["lobby_id"]
+        self.lobby_group_id = "lobby_%s" % self.lobby_id
 
-        await self.channel_layer.group_add(self.room_group_id, self.channel_name)
+        # Get user from the JWT token sent in the connection request
+        self.user = await get_user(self.scope)
+
+        await self.channel_layer.group_add(self.lobby_group_id, self.channel_name)
         await self.accept()
 
+        # await self.update_lobby_info()
+
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_group_id, self.channel_name)
+        await self.channel_layer.group_discard(self.lobby_group_id, self.channel_name)
 
     async def receive(self, text_data=None, **kwargs):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
+        message_type = text_data_json["type"]
 
-        await self.channel_layer.group_send(
-            self.room_group_id, {"type": "chat_message", "message": message}
-        )
+        match message_type:
+            case "chat":
+                await self.channel_layer.group_send(
+                    self.lobby_group_id, {"type": "chat_message", "message": text_data_json["message"]}
+                )
+            case "start_game":
+                pass
+            case "answer":
+                # # TODO: check answer, change self.scores
+                #
+                # if True:  # TODO: if everybody answered - show_result
+                #     channels_in_group = await self.channel_layer.group_channels(self.lobby_id)
+                #
+                #     for channel_name in channels_in_group:
+                #         await self.channel_layer.send(
+                #             channel_name, {'type': 'show_result', 'user_name': True, 'scores': self.scores}
+                #         )
+                #
+                #     # TODO: if game is not finished - next_round
+                pass
 
+    # async def start_game(self):
+    #     self.score = 0
+
+    # async def next_round(self, event):
+    #     message = event["message"]
+    #
+    #     await self.send(text_data=json.dumps({"type": "chat", "message": message}))
+
+    async def update_lobby_info(self):
+        channel_layer = get_channel_layer()
+        channels_in_group = await channel_layer.group_channels(self.lobby_id)
+
+        username_list = [channel.user.username for channel in channels_in_group]
+
+        for channel_name in channels_in_group:
+            await self.channel_layer.send(
+                channel_name, {'type': 'update_lobby_info', 'players': username_list}
+            )
+    
     async def chat_message(self, event):
         message = event["message"]
 
